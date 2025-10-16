12a827b8a7228e44b93921ab1ede530d468a7bb4
Pasha Stierle <pasha.king.stierle@gmail.com>
2025-10-16 13:23:46 -0700

    C++ File Creation
    I have initialized the C++ workflow with a custom movement component. You may need to Build from IDE.

---
+++ Source/GD73MegaJam2025/Public/CustomPawnMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "CustomPawnMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GD73MEGAJAM2025_API UCustomPawnMovementComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+	
+};
