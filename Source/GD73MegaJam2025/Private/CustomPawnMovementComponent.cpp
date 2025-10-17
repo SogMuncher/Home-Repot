44b3f35b732b2c090e07e159ee8b60793b1177f7
Pasha Stierle <pasha.king.stierle@gmail.com>
2025-10-17 16:53:11 -0700

    Half baked push

--- Source/GD73MegaJam2025/Private/CustomPawnMovementComponent.cpp
+++ Source/GD73MegaJam2025/Private/CustomPawnMovementComponent.cpp
@@ -2,4 +2,43 @@
 
 
 #include "CustomPawnMovementComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
+
+UCustomPawnMovementComponent::UCustomPawnMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	SetIsReplicatedByDefault(true);
+
+	PawnOwner = Cast<APawn>(GetOwner());
+
+	// Set up Capsule
+	Capsule = PawnOwner->FindComponentByClass<UCapsuleComponent>();
+
+	if (!Capsule)
+	{
+		Capsule = NewObject<UCapsuleComponent>(PawnOwner, TEXT("MovementCapsule"));
+		Capsule->RegisterComponent();
+		Capsule->SetupAttachment(PawnOwner->GetRootComponent());
+	}
+	
+	Capsule->SetCapsuleHalfHeight(45.f);
+	Capsule->SetCapsuleRadius(35.f);
+	Capsule->SetSimulatePhysics(true);
+	Capsule->SetEnableGravity(false);
+	Capsule->SetIsReplicated(true);
+
+}
+
+void UCustomPawnMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UCustomPawnMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
 
--- Source/GD73MegaJam2025/Public/CustomPawnMovementComponent.h
+++ Source/GD73MegaJam2025/Public/CustomPawnMovementComponent.h
@@ -9,9 +9,257 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(meta = (BlueprintSpawnableComponent))
 class GD73MEGAJAM2025_API UCustomPawnMovementComponent : public UPawnMovementComponent
 {
 	GENERATED_BODY()
 	
+public:
+
+	// 
+	// ===== CONSTRUCTOR & OVERRIDES ===== //
+	//
+
+	UCustomPawnMovementComponent();
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+
+	//
+	// ===== FUNCTIONS ===== //
+	//
+
+
+	// ===== Server ===== //
+
+	// Spring Force that keeps the Pawn off the ground
+	UFUNCTION(BlueprintAuthorityOnly)
+	void ApplySpringForce  (float DeltaTime);
+
+	// Movement Force applied to the Pawn on the X and Y axis
+	UFUNCTION(BlueprintAuthorityOnly)
+	void ApplyMovementForce(float DeltaTime);
+
+	// Gyroscope and Yaw Rotation of Pawn
+	UFUNCTION(BlueprintAuthorityOnly)
+	void ApplyRotationForce(float DeltaTime);
+
+	UFUNCTION(BlueprintAuthorityOnly)
+	void Jump();
+
+	UFUNCTION(BlueprintAuthorityOnly)
+	void UpdateTimers(float DeltaTime);
+
+	UFUNCTION(BlueprintAuthorityOnly)
+	bool ShouldSkipUpdate(float DeltaTime);
+
+
+	// ===== Remote Procedure Calls ===== //
+
+	UFUNCTION(Server, Unreliable, BlueprintCallable)
+	void Server_SendMoveInput(const FVector2D& MoveInput);
+
+	UFUNCTION(Server, Unreliable, BlueprintCallable)
+	void Server_SendControlRotation(const FRotator& ControlRotation);
+
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void Server_RequestJump();
+
+
+	// ===== Replication Notify Callbacks ===== //
+
+	UFUNCTION()
+	void OnRep_Velocity();
+
+	UFUNCTION()
+	void OnRep_bIsGrounded();
+
+	UFUNCTION()
+	void OnRep_bCanJump();
+
+	UFUNCTION()
+	void OnRep_bIsJumping();
+
+	UFUNCTION()
+	void OnRep_CapsuleTransform();
+
+	UFUNCTION()
+	void OnRep_LastValidMovementInputVector();
+
+
+	//
+	// ===== COMPONENTS ===== //
+	//
+
+	UPROPERTY(VisibleAnywhere, Category = "CustomPhysics|Components")
+	class UCapsuleComponent* Capsule;
+
+
+	//
+	// ===== TUNABLES ===== //
+	//
+
+
+	// ===== Spring Parameters ===== //
+
+	// Strength of Gravity applied to the character in units per second squared
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Spring")
+	float Gravity = 980.f;
+
+	// Strength of the Spring Force applied to the character
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Spring")
+	float SpringStrength = 85.f;
+
+	// Damping factor for the spring to reduce oscillation
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Spring")
+	float SpringDamping = 7.5f;
+
+	// Desired distance from the ground the spring will try to maintain
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Spring")
+	float SpringRideHeight = 120.f;
+
+	// Maximum distance the trace/ray can check for ground
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Spring")
+	float SpringLength = 150.f;
+
+	// Direction the spring will trace to find the ground
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Spring")
+	FVector SpringTraceDirection = FVector(0.f, 0.f, -1.f);
+
+	// Controls whether the spring can apply force towards the ground, sometimes undesirable. E.G. We want to tstick to the ground, but we do not want to accelerate towards it after losing grounded status
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Spring")
+	bool bAllowSpringDownwardForce = true;
+
+
+	// ===== Locomotion Parameters ===== //
+
+	// Maximum speed of the character
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Locomotion")
+	float MaxSpeed = 500.f;
+
+	// Acceleration rate applied when changing velocity
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Locomotion")
+	float Acceleration = 500.f;
+
+	// Maximum force allowed to be applied for movement
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Locomotion")
+	float MaxMovementForce = 15.f;
+
+	// Acceleration multiplier when moving against current velocity
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Locomotion")
+	UCurveFloat* TurnDotMultiplierCurve;
+
+
+	// ===== Rotation Parameters ===== //
+
+	// Speed at which the character rotates
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Rotation")
+	float RotationSpringStrength = 100.f;
+
+	// Damping factor for the yaw rotation to reduce oscillations
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Rotation")
+	float RotationSpringDamping = 5.f;
+
+	// Speed at which the character rotates back to upright
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Rotation")
+	float UprightSpringStrength = 100.f;
+
+	// Damping factor for the upright rotation to reduce oscillations
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Rotation")
+	float UprightSpringDamping = 10.f;
+
+	// Maximimum tilt amount applied to the character when moving fully against current velocity
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Rotation")
+	float MaxTiltDegrees = 60.f;
+
+
+	// ===== Jumping Parameters ===== //
+
+	// Force applied when the character jumps
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Jumping")
+	float JumpForce = 400.f;
+
+	// Air control multiplier applied when the character is in the air
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Jumping")
+	UCurveFloat* AirControlMultiplierCurve;
+
+	// Coyote Time - Time allowed to jump after leaving the ground
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Jumping")
+	float JumpCoyoteTime = 0.2f;
+
+	// Jump Buffer Time - Time allowed to jump after pressing the jump button before landing
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Jumping")
+	float JumpBufferTime = 0.1f;
+
+	// Jump Cooldown Time - Time before the next jump can be initiated
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CustomPhysics|Movement|Jumping")
+	float JumpCooldownTime = 0.2f;
+
+
+	//
+	// ===== STATE ===== //
+	//
+
+
+	// ===== Replicated ===== //
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomPhysics|State|Movement", Replicated)
+	FVector Velocity;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomPhysics|State|Movement", Replicated)
+	FVector GoalVelocity;
+
+	UPROPERTY(EditAnywhere,   BlueprintReadWrite, Category = "CustomPhysics|State|Movement", ReplicatedUsing = OnRep_bIsGrounded)
+	bool bIsGrounded = false;
+
+	UPROPERTY(EditAnywhere,   BlueprintReadWrite, Category = "CustomPhysics|State|Movement", Replicated)
+	bool bCanJump = true;
+
+	UPROPERTY(EditAnywhere,   BlueprintReadWrite, Category = "CustomPhysics|State|Movement", ReplicatedUsing = OnRep_bIsJumping)
+	bool bIsJumping = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomPhysics|State|Movement", Replicated)
+	float TimeSinceGrounded = 0.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomPhysics|State|Movement", Replicated)
+	float JumpCooldownTimer = 0.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomPhysics|State|Input",    Replicated)
+	FVector LastValidMovementInputVector;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CustomPhysics|State|Input",    Replicated)
+	FVector MovementInputNormalized;
+
+	UPROPERTY(EditAnywhere,   BlueprintReadWrite, Category = "CustomPhysics|State|Capsule",  Replicated)
+	FTransform CapsuleTransform;
+
+	UPROPERTY(EditAnywhere,   BlueprintReadWrite, Category = "CustomPhysics|State|Capsule",  Replicated)
+	FRotator CurrentBodyRotation;
+
+	UPROPERTY(EditAnywhere,    BlueprintReadOnly, Category = "CustomPhysics|State|Capsule",  Replicated)
+	FVector CurrentBodyForwardVector;
+
+
+	// ===== Authoratative inputs received from owning client ===== //
+
+	UPROPERTY(Transient)
+	FVector LastReceivedMoveInput = FVector::ZeroVector;
+
+	UPROPERTY(Transient)
+	FRotator LastReceivedControlRotation = FRotator::ZeroRotator;
+
+	UPROPERTY(Transient)
+	FVector2D LastReceivedCameraInput = FVector2D::ZeroVector;
+
+	UPROPERTY(Transient)
+	FRotator LastReceivedCameraRotation = FRotator::ZeroRotator;
+
+
+protected:
+
+
 };
+
